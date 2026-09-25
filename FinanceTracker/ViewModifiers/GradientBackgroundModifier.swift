89a83bae8cdeaf95e26dcbd908613279d8ea22c4
enzottic <tylermack2018@gmail.com>
2026-09-25 11:35:28 -0600

    show settings gradient on ipad now that settings is a tab

--- FinanceTracker/ViewModifiers/GradientBackgroundModifier.swift
+++ FinanceTracker/ViewModifiers/GradientBackgroundModifier.swift
@@ -34,18 +34,12 @@ extension View {
         modifier(GradientBackgroundModifier(color))
     }
 
-    /// Applies the Sage background treatment used by Settings lists.
-    @ViewBuilder
+    /// Applies the Sage background treatment used by Settings tab lists.
+    /// Sheets presented from Settings set their own `presentationBackground` instead.
     func settingsBackground() -> some View {
-        if UIDevice.current.userInterfaceIdiom != .pad {
-            scrollContentBackground(.hidden)
-                .textCase(nil)
-                .background(.sageBackground)
-                .gradientBackground()
-        } else {
-            scrollContentBackground(.hidden)
-                .textCase(nil)
-                .background(.sageBackground)
-        }
+        scrollContentBackground(.hidden)
+            .textCase(nil)
+            .background(.sageBackground)
+            .gradientBackground()
     }
 }
